bce931d1cd2ebc34f0bcf9fbab7561bcfc34476c
坂村俊 <s@skmr.jp>
2014-08-07 10:24:01 +0900

    テンプレートエンジンをjadeからECTに変更 npm install ect

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var ECT = require('ect');
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
 
 app.use(favicon());
 app.use(logger('dev'));
